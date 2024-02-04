d2f3204be01397c69f2c71e37152b5054597a196
gagal18 <gagaleskibojan@gmail.com>
2024-02-04 21:55:15 +0100

    feat: include example tests copy

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-function fetchAndDisplayTestResults() {
+function fetchAndDisplayResults() {
     const table = document.querySelector('.coderunner-test-results');
 
     if (!table) {
@@ -50,6 +50,52 @@ function fetchAndDisplayTestResults() {
         status.parentNode.appendChild(copy);
     }); 
 }
+function fetchAndDisplayExamples() {
+    const table = document.querySelector('.coderunnerexamples');
 
-fetchAndDisplayTestResults();
+    if (!table) {
+        console.error("Table not found.");
+        return;
+    }
+
+    const tbody = table.querySelector('tbody');
+
+    if (!tbody) {
+        console.error("Table body not found.");
+        return;
+    }
+
+    const testRows = tbody.querySelectorAll('tr');
+    testRows.forEach((testRow, id) => {
+        const columns = testRow.querySelectorAll("td");
+
+        // Create a new status column if not found
+        const newStatusCol = document.createElement('td');
+        testRow.insertBefore(newStatusCol, testRow.firstChild);
+
+        // Get the newly created or existing status column
+        const statusCol = testRow.querySelector('td:nth-child(1)');
+
+        const copy = document.createElement('button');
+        copy.textContent = 'Copy Test Case';
+        copy.className = 'copyTestCase';
+        copy.addEventListener('click', (e) => {
+            e.preventDefault()
+            const testColContent = testRow.querySelector("td:nth-child(2) pre");
+            const textarea = document.createElement('textarea');
+            textarea.value = testColContent.innerText;
+            document.body.appendChild(textarea);
+            textarea.select();
+            textarea.setSelectionRange(0, 99999);
+            navigator.clipboard.writeText(textarea.value+"\n");
+            document.body.removeChild(textarea);
+        });
+
+        statusCol.appendChild(copy);
+    }); 
+}
+
+fetchAndDisplayExamples();
+fetchAndDisplayResults();
+
 
